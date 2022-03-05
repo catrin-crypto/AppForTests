bde48549a750858f588ed79bb595fa0eac1f029e
catrin-crypto <78826785+catrin-crypto@users.noreply.github.com>
2022-03-05 18:32:55 +0300

    Lesson 5 ui automator Сделала переменные окружения, тест на открытие экрана настроек из методички не работает, комментарии в коде оставляю для себя, так как это мой учебный проект

--- app/src/androidTest/java/com/geekbrains/tests/espresso/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/espresso/MainActivityEspressoTest.kt
@@ -1,4 +1,4 @@
-package com.geekbrains.tests
+package com.geekbrains.tests.espresso
 
 import android.view.View
 import androidx.test.core.app.ActivityScenario
@@ -9,6 +9,8 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.geekbrains.tests.BuildConfig
+import com.geekbrains.tests.R
 import com.geekbrains.tests.view.search.MainActivity
 import org.hamcrest.Matcher
 import org.junit.After
